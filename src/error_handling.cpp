08833993d2ffa0bd1e72044e05b49b90ea9fe7f9
WillemSelten <WillemSelten>
2015-03-24 11:29:56 +0100

    Added few cases for errors

--- src/error_handling.cpp
+++ src/error_handling.cpp
@@ -10,7 +10,7 @@
 
 bool battery_empty;
 bool imu_error;
-
+bool laser_error_var;
 
 void ArduinoErrorHandler (const std_msgs::UInt8 &msg)
 {
@@ -23,6 +23,18 @@ void ArduinoErrorHandler (const std_msgs::UInt8 &msg)
 	}
 }
 
+void LaserErrorHandler(const std_msgs::UInt8 &msg)
+{
+	switch(msg.data)
+	{
+		case 1: laser_error_var = true;
+		break;
+		default:
+		laser_error_var = false;
+		break;
+	}
+}
+
 
 int main(int argc, char **argv)
 {
@@ -31,6 +43,7 @@ int main(int argc, char **argv)
   ros::NodeHandle n;
   ros::Publisher errors = n.advertise<std_msgs::UInt8>("error", 1000);
   ros::Subscriber arduino_errors = n.subscribe("arduino_error",1, &ArduinoErrorHandler);
+  ros::Subscriber laser_error = n.subscribe("lasererror",1,&LaserErrorHandler);
   ros::Rate loop_rate(10);
 
   
@@ -39,7 +52,12 @@ int main(int argc, char **argv)
     
     std_msgs::UInt8 msg;
     
+    if (laser_error_var == true) 
+    {
     msg.data = 255;
+    }
+    else msg.data = 0;
+    
 
 
     ROS_INFO("%i", msg.data);
